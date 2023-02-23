7ea51157b74624b5592134307a9bcd75e6a63e92
Anthony Josué Jara Bustamante <omarbush81@gmail.com>
2023-02-23 11:02:14 -0500

    Fix: COMPORTAMIENTO EXTRAÑO EN EL LOGIN
    (PV-19)[https://rodmensoft.atlassian.net/browse/PV-19]

--- lib/app/ui/pages/splash_page/splash_page.dart
+++ lib/app/ui/pages/splash_page/splash_page.dart
@@ -6,7 +6,6 @@ import 'package:siente_app/app/assets/assets.dart';
 import 'package:siente_app/app/ui/global_widgets/logo.dart';
 import 'package:siente_app/app/ui/pages/splash_page/splash_controller.dart';
 import 'package:siente_app/app/ui/theme/color_theme.dart';
-import 'package:simple_shadow/simple_shadow.dart';
 
 class SplashPage extends GetView<SplashController> {
   const SplashPage({super.key});
@@ -47,28 +46,20 @@ class TopSlogan extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Center(
-              child: SimpleShadow(
-                opacity: 0.6,
-                color: Colors.black, // Default: Black
-                offset: Offset(3.sp, 4.sp), // Default: Offset(2, 2)
-                sigma: 12.r,
-                child: LogoImage(
-                  width: 104.24.sp,
-                  height: 85.39.sp,
-                ), // Default: 2
+              child: Stack(
+                children: [
+                  LogoImage(
+                    width: 104.24.sp,
+                    height: 85.39.sp,
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 25.32.sp),
-            SimpleShadow(
-              opacity: 0.6,
-              color: Colors.black, // Default: Black
-              offset: Offset(3.sp, 4.sp), // Default: Offset(2, 2)
-              sigma: 12.r,
-              child: Image.asset(
-                Assets.assetsNewSplashSiente,
-                width: 189.66.sp,
-                height: 50.05,
-              ),
+            Image.asset(
+              Assets.assetsNewSplashSiente,
+              width: 189.66.sp,
+              height: 50.05,
             ),
             SizedBox(height: 11.32.sp),
             Text(
